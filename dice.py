35fe99554655b87a3f54a0aa85b84f35535a3a7b
David Keijser <keijser@gmail.com>
2019-02-27 16:12:41 +0100

    Use bag when constructing dice variable
    Speeds up the case with many dice at the cost of performance with few dice

--- dice.py
+++ dice.py
@@ -1,7 +1,7 @@
 import re
 import operator
 from functools import reduce
-from typing import Iterable
+from typing import Sequence
 
 from stochastic import Stochastic, Reduce, apply
 
@@ -18,22 +18,22 @@ def versus(a: int, b: int) -> int:
     return sign(a - b)
 
 
-def keep(vars: Iterable[Stochastic[int]], count: int) -> Stochastic[int]:
+def keep(bag: Stochastic[Sequence[int]], count: int) -> Stochastic[int]:
     fun: Reduce[int] = operator.add
 
-    def _keep(*v: int) -> int:
+    def _keep(v: Sequence[int]) -> int:
         return reduce(fun, sorted(v, reverse=True)[:count])
 
-    return apply(vars, _keep)
+    return apply([bag], _keep)
 
 
 def dice(spec: str) -> Stochastic[int]:
-    m = re.match(r'(\d)?d(\d+)(?:k(\d))?(?:\+(\d+))?', spec)
+    m = re.match(r'(\d+)?d(\d+)(?:k(\d))?(?:\+(\d+))?', spec)
     if m is None:
-        raise ValueError(f"Invalid format of dice spec `${spec}`")
+        raise ValueError(f"Invalid format of dice spec `{spec}`")
     count, size, k, offset = [int(v) if v else None for v in m.groups()]
     d = Stochastic.uniform(range(1, (size or 0) + 1))
     return (
-        keep([d] * (count or 1), k or count or 1)
+        keep(d.bag(count or 1), k or count or 1)
         + Stochastic.constant(offset or 0)
     )
